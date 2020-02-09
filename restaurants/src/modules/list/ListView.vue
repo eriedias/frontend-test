bae634e9e3401bf3f35437265d9f6722d9528072
Erie Dias <eriedias7@gmail.com>
2020-02-08 21:46:45 -0300

    Main header added

--- restaurants/src/modules/list/ListView.vue
+++ restaurants/src/modules/list/ListView.vue
@@ -1,5 +1,13 @@
 <template>
-    <h1>
-        List View
-    </h1>
+    <div>
+
+        <!-- Main Header -->
+        <header id="main-header">
+            <div class="container">
+                <h1>Restaurants</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tristique lorem sed mauris laoreet hendrerit.</p>
+            </div>
+        </header>
+
+    </div>
 </template>
